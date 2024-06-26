1db4038ac9fc552d381b8b7644d203d392320856
chaewon <89545462+gchaewon@users.noreply.github.com>
2024-06-26 18:01:59 +0900

    그림.cc

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,69 +1,62 @@
-#include <queue>
-#include <vector>
-#include <algorithm>
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
 
+#define X first;
+#define Y second;
 using namespace std;
+int n, m;
+vector<vector<int>> maps(501, vector<int> (501, -1));
+vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int bfs(int i, int j){
+    queue<pair<int, int>> q;
+    q.push({i, j});
+    int count = 0;
+    
+    while(!q.empty()){
+        int x = q.front().X;
+        int y = q.front().Y;
+        q.pop();
+        
+        for(auto d: dir){
+            int nx = x+d.X;
+            int ny = y+d.Y;
+            if(nx <0 || nx >=n || ny <0 || ny >=m) continue;
+            if(maps[nx][ny] !=1) continue;
+            maps[nx][ny] = 2; // 방문처리
+            q.push({nx, ny}); // 큐에 삽입
+            count++;
+        }
+    }
+    return count;
+}
 
 int main() {
-    int n, m;
     cin >> n >> m;
     vector<int> answer;
-    vector<vector<int>> v(n, vector<int> (m, 0));
-    
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            cin >> v[i][j];
-        }
-    }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(v[i][j]!=1){
-                continue;
-            }
-            else{
-                int count = 0;
-                queue<vector<int>> q;
-                q.push({i, j});
-                while(!q.empty()){
-                    int x = q.front()[0];
-                    int y = q.front()[1];
-                    q.pop();
-                    
-                    if(x+1<n && v[x+1][y] == 1){
-                        q.push({x+1, y});
-                        v[x+1][y] = 2;
-                        count++;
-                    }
-                    if(x-1>=0 && v[x-1][y] == 1){
-                        q.push({x-1, y});
-                        v[x-1][y] = 2;
-                        count++;
-                    }
-                    if(y+1<m && v[x][y+1] == 1){
-                        q.push({x, y+1});
-                        v[x][y+1] = 2;
-                        count++;
-                    }
-                    if(y-1>=0 && v[x][y-1] == 1){
-                        q.push({x, y-1});
-                        v[x][y-1] = 2;
-                        count++;
-                    }
-                }
-                answer.push_back(count);
-            }
+            cin >> maps[i][j];
         }
     }
     
-
-    if(answer.size() == 0){
-        cout << 0 << "\n" << 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(maps[i][j]!=1) continue;
+            answer.push_back(bfs(i, j));
+        }
     }
+    
+     if(answer.size() == 0){
+            cout << 0 << "\n" << 0;
+        }
     else{
         sort(answer.rbegin(), answer.rend());
         cout << answer.size() <<"\n";
         !answer[0] ? cout << 1: cout << answer[0];
     }
+    
     return 0;
 }
