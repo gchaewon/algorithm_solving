b76eb233dd88aa4855b2710fefa9103d7db28057
chaewon <89545462+gchaewon@users.noreply.github.com>
2024-07-04 16:16:51 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -4,25 +4,23 @@
 using namespace std;
 
 int main() {
-    int T;
-    vector<int> dp(12, 0);
-    \
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 4;
-    
-    for(int i=4; i<=11; i++){
-        for(int j=1; j<=3; j++){
-            dp[i] += dp[i-j];
-        }
+    int t;
+    cin >> t;
+    vector<int> dp(11, 0);
+
+    dp[1] = 1; // 1
+    dp[2] = 2; // 1+1, 2
+    dp[3] = 4; // 1+1+1, 2+1, 1+2, 3
+
+    for (int i = 4; i <= 10; i++) {
+        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
     }
-    cin >> T;
-    while(T--){
+
+    while (t--) {
         int n;
         cin >> n;
         cout << dp[n] << "\n";
     }
-    
+
     return 0;
 }
