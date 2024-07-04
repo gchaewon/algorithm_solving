340dd82a3129c169ebe0592b9c5ed0772961429d
chaewon <89545462+gchaewon@users.noreply.github.com>
2024-07-04 16:41:10 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -6,23 +6,22 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<int> v(n + 1, 0);
-    vector<int> dp(n + 1, 0);
+    vector<int> v(301, 0);
+    vector<vector<int>> dp(301, vector<int>(3, 0));
 
     for (int i = 1; i <= n; i++) {
         cin >> v[i];
     }
 
-    dp[0] = v[0];
-    dp[1] = v[1];
-    dp[2] = v[1] + v[2];
-    dp[3] = max(v[1], v[2]) + v[3];
+    dp[1][1] = v[1];
+    dp[1][2] = 0;
+    dp[2][1] = v[2];
+    dp[2][2] = v[1] + v[2];
 
-    for (int i = 4; i <= n; i++) {
-        dp[i] = max(dp[i - 3] + v[i - 1], dp[i - 2]) + v[i];
-        // cout << "dp[" << i << "] = " << dp[i] << "\n";
+    for (int i = 3; i <= n; i++) {
+        dp[i][1] = max(dp[i - 2][1], dp[i - 2][2]) + v[i];
+        dp[i][2] = dp[i - 1][1] + v[i];
     }
-    cout << dp[n];
 
-    return 0;
+    cout << max(dp[n][1], dp[n][2]);
 }
