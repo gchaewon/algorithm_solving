91b0f2f902cf5297f4ba190f598344158f2d02fd
chaewon <89545462+gchaewon@users.noreply.github.com>
2024-11-13 19:30:36 +0900

    쿼드트리.cc

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,42 +1,51 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
+
 int n;
-char arr[101][101];
-string s;
-string q(int y, int x, int size)
-{
-	if(size==1) return string(1,arr[y][x]);
-	string ret;
-	char b = arr[y][x];
-	for(int i=y; i<y+size; i++)
-	{
-		for(int j=x; j<x+size; j++)
-		{
-			if(b != arr[i][j])
-			{
-				ret += '(';
-				ret += q(y,x,size/2);
-				ret += q(y,x+size/2,size/2);
-				ret += q(y+size/2,x,size/2);
-				ret += q(y+size/2,x+size/2,size/2);
-				ret += ')';
-				return ret;
-			}
-		}
-	}
-	return string(1,arr[y][x]);
+int arr[65][65];
+
+void recur(int x, int y, int size) {
+    if (size == 1) {
+        cout << arr[x][y];
+        return;
+    }
+    int start = arr[x][y];
+    bool flag = true;
+
+    for (int i = x; i < x + size; i++) {
+        for (int j = y; j < y + size; j++) {
+            if (arr[i][j] != start) {
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    if (flag) {
+        cout << start;
+    }
+
+    else {
+        cout << "(";
+        recur(x, y, size / 2);
+        recur(x, y + (size / 2), size / 2);
+        recur(x + (size / 2), y, size / 2);
+        recur(x + (size / 2), y + (size / 2), size / 2);
+        cout << ")";
+    }
 }
-int main()
-{
-	cin >> n;
-	for(int i=0; i<n; i++)
-	{
-		cin >> s;
-		for(int j=0; j<n; j++)
-		{
-			arr[i][j] =s[j];
-		}
-	}
-	cout << q(0,0,n);
-	return 0;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < n; j++) {
+            arr[i][j] = s[j] - '0';
+        }
+    }
+    recur(0, 0, n);
+    return 0;
 }
